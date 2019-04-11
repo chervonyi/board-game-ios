341b596ecedcc3c51e419300f42e3ace80d6d085
chervonyi <yuri.ternopil@gmail.com>
2019-04-11 12:52:39 -0500

    Import of images

---
+++ BoardGame/BoardGame/Views/BoardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoardGame
+//
+//  Created by Yuri Chervonyi on 4/11/19.
+//  Copyright © 2019 CHRGames. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
